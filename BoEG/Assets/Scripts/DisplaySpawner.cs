dbc48baeb18412bb4ef6cf7153c50146424b5311
Marcus Kertesz <ModernMAK@Hotmail.com>
2020-12-22 20:29:41 -0800

    Playing With Shaders

---
+++ BoEG/Assets/Scripts/DisplaySpawner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Developer.PlayTesting
+{
+    [ExecuteInEditMode]
+    public class DisplaySpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _prefab;
+        [SerializeField] private bool _spawn;
+        [SerializeField] private bool _clear;
+
+
+        private void Update()
+        {
+            if (_spawn && _prefab != null)
+            {
+                _spawn = false;
+                SpawnPrefabs();
+            }
+
+            if (_clear)
+            {
+                _clear = false;
+                ClearChildren();
+            }
+        }
+
+        private void ClearChildren()
+        {
+            foreach (Transform child in transform)
+            {
+                foreach (Transform subchild in child)
+                {
+                    DestroyImmediate(subchild.gameObject);
+                }
+            }
+        }
+
+        private void SpawnPrefabs()
+        {
+            foreach (Transform child in transform)
+            {
+                foreach (Transform subchild in child)
+                {
+                    DestroyImmediate(subchild.gameObject);
+                }
+
+                var prefab = PrefabUtility.InstantiatePrefab(_prefab) as GameObject;
+
+                prefab.transform.SetParent(child, false);
+                prefab.transform.localPosition = Vector3.zero;
+
+//                prefab.transform.localPosition = Vector3.zero;
+            }
+        }
+    }
+}
---
+++ BoEG/Assets/Scripts/TeamableTinter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Modules.Teamable;
+using UnityEngine;
+
+[RequireComponent(typeof(ITeamable))]
+public class TeamableTinter : MonoBehaviour
+{
+    private ITeamable _teamable;
+    private List<Material> _materials;
+    private static readonly int TeamTint = Shader.PropertyToID(ShaderKey);
+
+    private const string ShaderKey = "_TeamTint";
+
+    private void Awake()
+    {
+        _materials = new List<Material>();
+        foreach (var mr in GetComponentsInChildren<MeshRenderer>())
+            _materials.Add(mr.material);
+
+        _teamable = GetComponent<ITeamable>();
+        _teamable.TeamChanged += TeamableOnTeamChanged;
+    }
+
+    private void Apply(Color color)
+    {
+        foreach (var mat in _materials)
+            mat.SetColor(TeamTint, color);
+    }
+
+    private void TeamableOnTeamChanged(object sender, TeamData e)
+    {
+        Apply(e != null ? e.GetTint() : Color.white);
+    }
+}
