88b177f80401dc8c36151efd2c43895993579ac0
Marcus Kertesz <ModernMAK@Hotmail.com>
2021-02-15 01:47:37 -0800

    Reorg/Delete Depricated

---
+++ BoEG/Assets/Scripts/Framework/Core/Trigger/TriggerHelper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MobaGame.Framework.Core.Trigger
+{
+	public class TriggerHelper<T> where T : Collider
+    {
+        public T Collider { get; set; }
+        public Trigger Trigger { get; set; }
+    }
+}
